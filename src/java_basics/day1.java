07ec018b059507af9b718b13599e79d644e68daf
Prem-Yelwande <premyelwande1105@gmail.com>
2026-06-27 17:11:26 +0530

    first basic program

--- src/java_basics/day1.java
+++ src/java_basics/day1.java
@@ -1,7 +1,14 @@
 package java_basics;
+import java.util.Scanner;
 
 public class day1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // needed for user input
+
+        double r = sc.nextInt();
+        double a = 2*(3.142) * r;
+        System.out.println(a);
+
 
     }
 }
